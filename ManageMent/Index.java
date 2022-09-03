e0aa36cec1c4eb7d78dc863d3abb976eaaa8e9a7
Polobear9 <lieams1996@gmail.com>
2022-09-04 00:03:54 +0900

    System Adjust the code to simply and More detail.

--- ManageMent/Index.java
+++ ManageMent/Index.java
@@ -2,26 +2,24 @@ package ManageMent;
 
 
 public class Index {
-	
-	public static void main(String[] args) {
-		// Set the Member , Company Name , Member name , Member rank , Age
-		Member member1 = new Member("Nexon", "Lee", "polobear", 2, 27);
-		Companylaborcontract comlabor1 = new Companylaborcontract("Full-Time");
-		
-		System.out.print(member1.MemberFirstName + " " + member1.MemberLastName);
-		
-		System.out.println(comlabor1.RestTime + " " + comlabor1.LaborworkTime);
 
-		System.out.println(member1.salary);
-		
-		int baseincome = (member1.salary / 22 / comlabor1.LaborworkTime);
-		int Fullsalary = baseincome * 22;
-		System.out.println("This Member is Fullsalary is " + Fullsalary + " Japan Yen.");
-		System.out.println("This Member is Baseincome is " + baseincome + " Japan Yen.");
-		System.out.println("This Member work " + comlabor1.LaborworkTime * 22 + " Hours.");
-		
-		
-		
-	}
+    public static void main(String[] args) {
+        // Set the Member , Company Name , Member name , Member rank , Age
+        Member member1 = new Member("Nexon", "Lee", "polobear", 2, 27);
+        Companylaborcontract comlabor1 = new Companylaborcontract("Full-Time");
+
+        System.out.print(member1.getMemberFirstName() + " " + member1.getMemberLastName());
+        System.out.println(comlabor1.getRestTime() + " " + comlabor1.getLaborworkTime());
+        System.out.println(member1.getSalary());
+
+        int baseincome = (member1.getSalary() / 22 / comlabor1.getLaborworkTime());
+        int Fullsalary = baseincome * 22;
+
+        System.out.println("This Member is Fullsalary is " + Fullsalary + " Japan Yen.");
+        System.out.println("This Member is Baseincome is " + baseincome + " Japan Yen.");
+        System.out.println("This Member work " + comlabor1.getLaborworkTime() * 22 + " Hours.");
+
+
+    }
 
 }
--- ManageMent/Member.java
+++ ManageMent/Member.java
@@ -1,39 +1,84 @@
 package ManageMent;
 
 public class Member {
-	
 
-	String ComName;
-	String MemberFirstName;
-	String MemberLastName;
-	int CompanyRank;
-	int Age;
-	int salary;
-	
+
+	private String ComName;
+	private String MemberFirstName;
+	private String MemberLastName;
+	private int CompanyRank;
+	private int Age;
+	private int salary;
+
 	public Member(String ComName, String MemberFirstName, String MemberLastName, int CompanyRanks, int Age) {
 		this.ComName = ComName;
 		this.MemberFirstName = MemberFirstName;
 		this.MemberLastName = MemberLastName;
 		this.CompanyRank = CompanyRanks;
 		this.Age = Age;
-		
-		switch(CompanyRank) {
-		//Set Base Salary each rank.
-		case 1:
-			this.salary = 350000;
-			return;
-		case 2:
-			this.salary = 250000;
-			return;
-		case 3:
-			this.salary = 200000;
-		default:
-			this.salary = 180000;
-			return;
-		
+
+		switch (CompanyRank) {
+			//Set Base Salary each rank.
+			case 1:
+				this.salary = 350000;
+				return;
+			case 2:
+				this.salary = 250000;
+				return;
+			case 3:
+				this.salary = 200000;
+			default:
+				this.salary = 180000;
+				return;
+
 		}
 	}
-		
-		
-		
+
+	public String getComName() {
+		return ComName;
+	}
+
+	public void setComName(String comName) {
+		ComName = comName;
+	}
+
+	public String getMemberFirstName() {
+		return MemberFirstName;
+	}
+
+	public void setMemberFirstName(String memberFirstName) {
+		MemberFirstName = memberFirstName;
+	}
+
+	public String getMemberLastName() {
+		return MemberLastName;
+	}
+
+	public void setMemberLastName(String memberLastName) {
+		MemberLastName = memberLastName;
+	}
+
+	public int getCompanyRank() {
+		return CompanyRank;
+	}
+
+	public void setCompanyRank(int companyRank) {
+		CompanyRank = companyRank;
+	}
+
+	public int getAge() {
+		return Age;
+	}
+
+	public void setAge(int age) {
+		Age = age;
+	}
+
+	public int getSalary() {
+		return salary;
+	}
+
+	public void setSalary(int salary) {
+		this.salary = salary;
+	}
 }
